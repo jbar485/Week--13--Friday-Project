ca681239460781a4e369f0d103323489d2c7e4eb
Judah Barton <judah.barton@yahoo.com>
2020-01-31 09:59:59 -0800

    add a teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,35 @@
 class TeamsController < ApplicationController
 
   def index
-    @teams = {"Team": "Seattle Seahawks"}
+    @teams = Team.all
     json_response(@teams)
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    json_response(@team)
+  end
+
+  def create
+    @team = Team.create!(team_params)
+    json_response(@team)
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      render status: 200, json: {
+        team: "Your update was successful"
+      }
+    end
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def team_params
+    params.permit(:name, :founded, :stadium, :city, :state, :logo)
   end
 end
