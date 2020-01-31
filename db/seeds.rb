23e18f9bb975efad5b34067e5964f7b354922be3
Judah Barton <judah.barton@yahoo.com>
2020-01-31 09:49:42 -0800

    seed the database with some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.destroy_all
+Player.destroy_all
+
+seahawks = Team.create!(
+  name: "Seattle Seahawks",
+  logo: "https://toppng.com/public/uploads/preview/19-beautiful-nfl-teams-logos-seattle-seahawks-logo-transparent-11563199254zonapfkvjz.png",
+  state: "Washington",
+  city: "Seattle",
+  stadium: "Century Link Field",
+  founded: 1974
+)
+chiefs = Team.create!(
+  name: "Kansas City Chiefs",
+  logo: "https://banner2.cleanpng.com/20180714/tfq/kisspng-arrowhead-stadium-kansas-city-chiefs-nfl-kansas-ci-chief-5b49f33bdd5735.2962382815315730519066.jpg",
+  state: "Missouri",
+  city: "Kansas City",
+  stadium: "Arrowhead Stadium",
+  founded: 1960
+)
+packers = Team.create!(
+  name: "Green Bay Packers",
+  logo: "https://library.kissclipart.com/20181003/fxe/kissclipart-green-bay-packers-logo-clipart-green-bay-packers-l-f6c1844a7039223e.jpg",
+  state: "Wisconsin",
+  city: "Green Bay",
+  stadium: "Lambeau Field",
+  founded: 1919
+)
